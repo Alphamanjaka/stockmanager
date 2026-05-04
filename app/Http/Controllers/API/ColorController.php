599b656149e8ec8d7af444123c8a21164067ca73
alphamanjaka <alphamanjaka@gmail.com>
2026-05-04 13:49:09 +0300

    feat: Implement ColorController and ColorResource with CRUD functionality

---
+++ app/Http/Controllers/API/ColorController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreColorRequest;
+use App\Http\Resources\ColorResource;
+use App\Models\Color;
+use App\Services\ColorService;
+
+class ColorController extends Controller
+{
+    public function __construct(private ColorService $color_service )
+    {
+
+    }
+    // display a listing of the resource
+    public function index()
+    {
+        // return a collection of colors
+        return ColorResource::collection(Color::paginate(15));
+    }
+    // store a newly created resource in storage
+    public function store(StoreColorRequest $request)
+    {
+        // validate the request data
+        $validated = $request->validated();
+        // create a new color
+        $color = $this->color_service->create($validated);
+        // return the created color as a resource
+        return new ColorResource($color);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\ColorController;
 use Illuminate\Support\Facades\Route;
 
 // Registers standard RESTful endpoints for products
 Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/colors', ColorController::class);
